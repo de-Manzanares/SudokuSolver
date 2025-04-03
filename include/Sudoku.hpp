ca8b25d5bb34d5cbc5d05d29e228c4469bdca25d
schauss <schauss@dmanz.org>
2025-04-02 18:08:30 -0700

    solve.cpp

--- include/Sudoku.hpp
+++ include/Sudoku.hpp
@@ -82,46 +82,6 @@ class Sudoku {
   void initialize_candidates(int cell);
 };
 
-inline void Sudoku::solve() {
-  initialize_candidates();
-  print_candidates("Candidates:\n", _candidates);
-
-  bool progress = true;
-  while (progress) {
-    progress = false;
-
-    if (solve_explicit_singles()) {
-      std::cout << '\n';
-      print_puzzle();
-      progress = true;
-      continue;
-    }
-
-    if (find_implicit_singles()) {
-      solve_implicit_singles();
-      std::cout << '\n';
-      print_puzzle();
-      progress = true;
-      continue;
-    }
-
-    if (prune_pointing_locked_candidates()) {
-      progress = true;
-      continue;
-    }
-
-    if (prune_claiming_locked_candidates()) {
-      progress = true;
-      continue;
-    }
-
-    // if (prune_hidden_subsets()) {
-    //   progress = true;
-    //   continue;
-    // }
-  }
-}
-
 // begin - prune candidates ----------------------------------------------------
 
 // hidden pair/triple/quad
